7826ca2a56a46d59a8e790dc5544eb60a01d0f4d
valtyrmar <valtyrmm@gmail.com>
2024-11-12 10:22:59 +0100

    added play_game function to see if code runs

--- hangman.py
+++ hangman.py
@@ -7,6 +7,7 @@ import random
 import getpass
 
 #select a word for the other player to guess
+#getpass hides the word from the guesser
 def select_random_word():
     word = getpass.getpass("Enter a word: ").lower()
     return word
@@ -18,3 +19,10 @@ def start_game(word):
             'guessed_letters': set(),
             'attempts': 8
             }
+
+#play the game
+def play_game():
+    word = select_random_word()
+    game = start_game(word)
+
+    print("let's play hangman!")
