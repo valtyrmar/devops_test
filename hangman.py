d94aeffc046d2406f1ae322b4fcc11f780ea7de3
valtyrmar <valtyrmm@gmail.com>
2024-11-12 10:09:25 +0100

    created select_word function

--- hangman.py
+++ hangman.py
@@ -2,3 +2,10 @@
 This is a hangman game developed for entertainment purposes :)
 Author: Valtýr Már Michaelsson
 """
+
+import random
+import getpass
+
+def select_random_word():
+    word = getpass.getpass("Enter a word: ").lower()
+    return word
